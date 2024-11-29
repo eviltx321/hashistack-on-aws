57dcdc62308d96722ccc14a56c30d91ec9e7052a
pastorismylord <53154863+pastorismylord@users.noreply.github.com>
2024-11-29 12:25:24 -0600

    mor var stuff

--- packages/variables.tf
+++ packages/variables.tf
@@ -171,9 +171,9 @@ data "terraform_remote_state" "infrastructure" {
 }
 
 locals {
-  aws_lb_controller_role_arn      = var.aws_lb_controller_role_arn        = try(data.terraform_remote_state.infrastructure.outputs.aws_lb_controller_role_arn, var.aws_lb_controller_role_arn)
-  pod_cloudwatch_log_group_name   = var.od_cloudwatch_log_group_name    = try(data.terraform_remote_state.infrastructure.outputs.pod_cloudwatch_log_group_name, var.pod_cloudwatch_log_group_name)
-  pod_cloudwatch_logging_arn      = var.pod_cloudwatch_logging_arn      = try(data.terraform_remote_state.infrastructure.outputs.pod_cloudwatch_logging_arn, var.pod_cloudwatch_logging_arn)
+  aws_lb_controller_role_arn      = var.aws_lb_controller_role_arn        == try(data.terraform_remote_state.infrastructure.outputs.aws_lb_controller_role_arn, var.aws_lb_controller_role_arn)
+  pod_cloudwatch_log_group_name   = var.od_cloudwatch_log_group_name    == try(data.terraform_remote_state.infrastructure.outputs.pod_cloudwatch_log_group_name, var.pod_cloudwatch_log_group_name)
+  pod_cloudwatch_logging_arn      = var.pod_cloudwatch_logging_arn      == try(data.terraform_remote_state.infrastructure.outputs.pod_cloudwatch_logging_arn, var.pod_cloudwatch_logging_arn)
   eks_cluster_name                  = try(data.terraform_remote_state.infrastructure.outputs.eks_cluster_name, var.eks_cluster_name)
   eks_oidc_provider_arn             = try(data.terraform_remote_state.infrastructure.outputs.eks_oidc_provider_arn, var.eks_oidc_provider_arn)
   eks_cluster_api_endpoint          = try(data.terraform_remote_state.infrastructure.outputs.eks_cluster_api_endpoint, var.eks_cluster_api_endpoint)
